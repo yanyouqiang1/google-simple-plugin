423e4e433ea6f8dd7bc70e05db521bc7355f4906
yanyouqiang <374580618@qq.com>
2022-07-21 11:13:47 +0800

    整理pop页面的名称

--- popup.js
+++ popup.js
@@ -2,10 +2,6 @@ $("#base64").click(function (){
     window.open("base64/base64.html")
 })
 
-$("#url-store").click(function (){
-    window.open("url-store/urlstore.html")
-})
-
 $("#html-str-show").click(function (){
     window.open("html-str-show/html-str-show.html")
 })
